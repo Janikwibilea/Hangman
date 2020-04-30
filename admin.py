c487f51dbf8d99c515e450674cad7edc4696c530
Rag <rag@Rag-VM-Ubuntu-20.04>
2020-04-30 14:48:53 +0200

    a

--- admin.py
+++ admin.py
@@ -11,11 +11,18 @@ class manage_session:
 
     def close_file(self):
         self.file.close()
-    
+
+    def write_file(self, text):
+        self.open_file("w")
+        self.file.write(text)
+        self.close_file()
+
     def start(self):
         self.open_file("w")
         self.file.write('"Waiting for Player...", "Username of Admin: {}", 0'.format(self.user_name))
         self.close_file()
+        """ self.write_file('"Waiting for Player...", "Username of Admin: {}", 0'.format(self.user_name))
+        print(Fore.BLUE + "Game Created, waiting for other Player") """
 
     def state(self):
         self.open_file("r")
@@ -23,26 +30,26 @@ class manage_session:
         self.close_file()
         return self.session_state
 
-def waiting_for_user():
-    user_is_ready = False
-    while user_is_ready == False:
-        if session.state() == 1:
-            print(Fore.GREEN + "User is ready")
-            user_is_ready = True
-        elif session.state() == 0:
-            print(Fore.YELLOW + "User is not ready")
-            time.sleep(1)
-        else:
-            print(Fore.RED + "Something went wrong, exiting Hangman")
-            sys.exit(0)
+    def waiting_for_user(self):
+        user_is_ready = False
+        while user_is_ready == False:
+            if session.state() == 1:
+                print(Fore.GREEN + "User is ready")
+                user_is_ready = True
+            elif session.state() == 0:
+                print(Fore.YELLOW + "User is not ready")
+                time.sleep(1)
+            else:
+                print(Fore.RED + "Something went wrong, exiting Hangman")
+                sys.exit(0)
 
 def input_secret_word():
-    # Input secret word.
-    # Print to session.txt
-    pass
+    secret_word = input("Input secred Word: ")
+    word_level = len(secret_word)
+    session.write_file("{}, {}".format(secret_word, word_level))
 
 def user_stats():
-    # Importing stats from stats.txt
+    # Importing stats from stats.txt 
     # TODO: Creating stats.txt
     # Print stats until game finish
     pass
@@ -62,6 +69,7 @@ def hangman_main():
     global session
     session = manage_session("connection_files/session.txt", user_name)
     session.start()
+    session.waiting_for_user()
     input_secret_word()
     user_stats()
     get_results()
